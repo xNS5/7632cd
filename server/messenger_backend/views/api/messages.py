307c489a5fa5d366cab0dbe1a3558f0b641376c4
Michael Kennedy <michaelkennedyns5@gmail.com>
2022-03-11 18:57:45 -0800

    Adding put function to message API and adding boolean field to model

--- server/messenger_backend/views/api/messages.py
+++ server/messenger_backend/views/api/messages.py
@@ -4,9 +4,14 @@ from messenger_backend.models import Conversation, Message
 from online_users import online_users
 from rest_framework.views import APIView
 
+"""
+TODO: write PATCH/PUT request to update the message in the given conversation.
+https://www.restapitutorial.com/lessons/httpmethods.html
+"""
+
 
 class Messages(APIView):
-    """expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)"""
+    """expects {recipientId, text, readStatus, conversationId } in body (conversationId will be null if no conversation exists yet)"""
 
     def post(self, request):
         try:
@@ -32,7 +37,7 @@ class Messages(APIView):
                 message_json = message.to_dict()
                 return JsonResponse({"message": message_json, "sender": body["sender"]})
 
-            # if we don't have conversation id, find a conversation to m       ake sure it doesn't already exist
+            # if we don't have conversation id, find a conversation to make sure it doesn't already exist
             conversation = Conversation.find_conversation(sender_id, recipient_id)
             if not conversation:
                 # create conversation
@@ -47,4 +52,21 @@ class Messages(APIView):
             message_json = message.to_dict()
             return JsonResponse({"message": message_json, "sender": sender})
         except Exception as e:
-            return HttpResponse(status=500)
+            return HttpResponse(content={"error": e}, status=500)
+
+    def put(self, request):
+        try:
+            user = get_user(request)
+
+            if user.is_anonymous:
+                return HttpResponse(status=401)
+
+            sender_id = user.id
+            body = request.data
+            conversation_id = body.get("conversationId")
+            recipient_id = body.get("recipientId")
+            sender = body.get("sender")
+            read = body.get("read")
+
+        except Exception as e:
+            return HttpResponse(content={"error": e}, status=500)
