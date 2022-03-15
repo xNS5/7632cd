84eebaefbaf423f70b6359a2c885fd99d8eeece2
Michael Kennedy <michaelkennedyns5@gmail.com>
2022-03-14 22:58:35 -0700

    Removing function from ActiveChat

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -37,14 +37,6 @@ const ActiveChat = ({
     return obj !== {} && obj !== undefined;
   };
 
-  const markRead = async (conversation) => {
-    try{
-
-    } catch(e) {
-      console.log(e)
-    }
-  }
-
   return (
     <Box className={classes.root}>
       {isConversation(conversation) && conversation.otherUser && (
