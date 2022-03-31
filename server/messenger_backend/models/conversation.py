8286826402d24952822006e5f0569bb6f0f54355
Michael Kennedy <michaelkennedyns5@gmail.com>
2022-03-31 13:39:36 -0700

    Putting user1 and user2 back into the model

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -1,9 +1,28 @@
 from django.db import models
 
 from . import utils
+from .user import User
 
 
 class Conversation(utils.CustomModel):
 
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
+
+    user1 = models.ForeignKey(
+        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
+    )
+    user2 = models.ForeignKey(
+        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+",
+    )
+
+    # find conversation given two user Ids
+    def find_conversation(user1Id, user2Id):
+        # return conversation or None if it doesn't exist
+        try:
+            return Conversation.objects.get(
+                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
+                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
+            )
+        except Conversation.DoesNotExist:
+            return None
